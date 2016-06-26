cba19070269183ed4d65be51c03f188406e7c092
qingfan <296200074@qq.com>
2016-06-26 14:25:49 +0800

    7.0 by qingfan

--- static/js/page/news_Detail.js
+++ static/js/page/news_Detail.js
@@ -6,21 +6,21 @@ require.config({
 	baseUrl:MIS.STATIC_ROOT
 });
 require(['lib/jquery', 'util/request','util/funcTpl','lib/juicer'], function($, request,funcTpl) {
-    var newsId;
+    var newsId,
+        page,
+        totalPages,
+        startPage;
 	var new_Detail={
 
 		init:function(){
 
-	        //new_Detail.getStorgeId();
 	        new_Detail.getDetailId();
 	        new_Detail.getOneNews();
-
 	        
 		},
 
 		getDetailId:function(){
               newsId=localStorage.getItem("newsId");
-              console.log(newsId);
 		},
 
 		getOneNews:function(){
@@ -37,34 +37,28 @@ require(['lib/jquery', 'util/request','util/funcTpl','lib/juicer'], function($, 
                 	    content,
                 	    count,
                 	    start,
-                	    end,
-                	    page,
-                	    totalPages,
-                	    startPage;
+                	    end;
 
-                	//获取新闻内容
                     content=res.data.news.content;
-
                     start=0;
-                    page=0;
-                    count=2000;
-                    temp=2000;
-                    totalPages=0;
-                    startPage=0;
+                    page=1;
+                    count=1772;
+                    temp=1772; 
+                    startPage=1;
                     end=content.toString().length;
                     totalPages=Math.ceil(end/count);
-                    console.log(totalPages);
-                 
+                    var pageInput=$(".footer > p").find(".page");
+
                     for(var i = 0; i < totalPages; i++){
                     	if(i == totalPages-1){
 	                        caseNewsArr.push(content.substring(start,end));
                     	}else{
                     		caseNewsArr.push(content.substring(start,temp));
                     	}
-                    	temp +=count
+                    	temp +=count;
 	                    start += count;
                     }
-                    console.log(caseNewsArr);
+
                     var caseNewsArrData = {
 
                     	"caseNewsList":caseNewsArr
@@ -77,21 +71,75 @@ require(['lib/jquery', 'util/request','util/funcTpl','lib/juicer'], function($, 
 
                     var caseNewTpl=juicer(funcTpl(new_Detail.cTpl),caseNewsArrData);
                     $(".content").append(caseNewTpl);
-                    new_Detail.newsPage(page);
 
                     $(".footer").append(juicer(funcTpl(new_Detail.footerTpl),pageData));
-                    //new_Detail.footer();
+                    new_Detail.turnCss();                    
+                    pageInput.val(page);
+                    new_Detail.newsPage(page);
+                    
+                    new_Detail.turnPage();
                 }
 	        );
 		},
 
 		//新闻页面显示
 		newsPage:function(page){
-			console.log(page);
+			
 			var newsPages=$(".news_content");
-			console.log(newsPages);
-			newsPages.eq(page).show().siblings().hide();
+			
+			newsPages.eq(page-1).show().siblings().hide();
+			$(".footer > p").find(".page").val(page);
+		},
 
+		turnPage:function(){
+
+			$("#font").click(function(){
+                				
+				new_Detail.newsPage(new_Detail.lastPage());
+			});
+
+			$("#behind").click(function(){
+				
+				new_Detail.newsPage(new_Detail.nextPage());
+			});
+		},
+
+		nextPage:function(){
+            $("#behind").css({"color":"#060606"});
+	        /* 判断是否到最后一页 */
+			if(page == totalPages&&page == startPage) {
+                 
+				$("#behind").css({"color":"#e3e3e3","cursor":"not-allowed"});
+				$("#font").css({"color":"#e3e3e3","cursor":"not-allowed"});				
+				return totalPages;
+
+			} else if(page == totalPages && page !== startPage){
+                $("#behind").css({"color":"#e3e3e3","cursor":"not-allowed"});
+                return totalPages;
+			}else {
+				$("#font").css({"color":"#060606","cursor":"default"});
+				return ++ page;
+			}
+		},
+
+		lastPage:function(){
+	       $("#font").css({"color":"#060606"});
+
+	        /* 判断是否到初始页 */
+			if(page == startPage && page == totalPages) {
+                $("#behind").css({"color":"#e3e3e3","cursor":"not-allowed"});
+				$("#font").css({"color":"#e3e3e3","cursor":"not-allowed"});	
+				return startPage;
+
+			} else if(page == startPage && page !== totalPages){
+               $("#font").css({"color":"#e3e3e3","cursor":"not-allowed"});	
+               return startPage;
+
+			}else {
+				$("#behind").css({"color":"#060606","cursor":"default"});
+				return -- page;
+			}
+            		
 		},
 
 		detailTpl:function(){
@@ -120,8 +168,8 @@ require(['lib/jquery', 'util/request','util/funcTpl','lib/juicer'], function($, 
 				                ${newsCase}
 			                </p>
 		            </div>
-
 		        {@/each}
+
 	        </div>
 	        */
 		},
@@ -130,53 +178,44 @@ require(['lib/jquery', 'util/request','util/funcTpl','lib/juicer'], function($, 
 
 			/*跳转页面样式*/
 	        $("#font").hover(function(){
-	            $(this).css({"color":"#fff","text-decoration":"underline"});
+	        	if(page == startPage){
+
+	        	}else{
+	        		$(this).css({"color":"#fff"});
+	        	}
+	            
 	        },function(){
-	            $(this).css({"color":"#060606","text-decoration":"none"});
+	        	if(page==startPage){
+
+	        	}else{
+	        		$(this).css({"color":"#060606"});
+	        	}
+	            
 	        });
 
 	        $("#behind").hover(function(){
-	            $(this).css({"color":"#fff","text-decoration":"underline"});
+	        	if(page == totalPages){
+
+	        	}else{
+	        		$(this).css({"color":"#fff"});
+	        	}
+	            
 	        },function(){
-	            $(this).css({"color":"#060606","text-decoration":"none"});
+	        	if(page==totalPages){
+                    
+	        	}else{
+	        		$(this).css({"color":"#060606"});
+	        	}
+	            
 	        });
             
-		},
-
-		nextPage:function(){
-            $("#behind").css({"color":"#060606"});
-
-	        /* 判断是否到最后一页 */
-			if(page == totalPages) {
-
-				$("#behind").css({"color":"#e3e3e3","curosr":"not-allowed"});				
-				return totalPages;
-
-			} else {
-				return ++ page;
-			}			
-	        
-		},
-
-		lastPage:function(){
-	        $("#font").css({"color":"#060606"});
-
-	        /* 判断是否到初始页 */
-			if(page == startPage) {
-
-				$("#font").css({"color":"#e3e3e3","curosr":"not-allowed"});	
-				return startPage;
-			} else {
-				
-				return -- page;
-			}			
 		},
 
 		footerTpl:function(){
 			/*
 				<p>
 					<span class="text" id="font">上一页</span>
-					<span class="text">第 <span id="at"></span> 页</span>
+					<span class="text">第<input class="page">页</span>
 					<span class="text" id="behind">下一页</span>
 					<span class="text-1">共 ${casePage} 页</span>
 				</p>
